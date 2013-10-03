74781a49cd94cf3f67bca0b5ce7b378e0a2b75df
Michael Herman <michael@mherman.org>
2013-10-02 18:02:59 -0600

    push

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	// event handler on submit
+	$('.btn-block').click(function() {
+
+		// set variables
+    var bio 	= $('.form-one').text(); 
+   	var books = $('.form-two').val();
+   	var libs 	= $('.form-three').val(); 
+
+
+	console.log(bio)
+
+		});
+
+}
